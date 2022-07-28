f3f9a1a71c0526ebb2c36d5940a0a796a0b7f037
Isaacjuega <94757601+Isaacjuega@users.noreply.github.com>
2022-07-28 16:57:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+ import * as firebase from "firebase";
+
+//inicializa tu base de datos
+  const firebaseConfig = {
+  apiKey: "AIzaSyBRqWqSJxQLINn6-Uk_MW3hhEKVdbm9pZA",
+  authDomain: "proyecto-60-6bdd7.firebaseapp.com",
+  databaseURL: "https://proyecto-60-6bdd7-default-rtdb.firebaseio.com",
+  projectId: "proyecto-60-6bdd7",
+  storageBucket: "proyecto-60-6bdd7.appspot.com",
+  messagingSenderId: "535828836977",
+  appId: "1:535828836977:web:11a37d8d1231cbb59d61d0",
+  measurementId: "G-FMP75DB9WZ"
+};
+
+  export default firebase.database()
+ 
+// Me marca error pero no veo donde lo marca
+  
